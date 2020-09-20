8c91cee4de27e46cb9751de952d672cf72963a18
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-09-20 17:48:15 -0300

    base Work

--- index.js
+++ index.js
@@ -2,12 +2,54 @@ const { ApolloServer,gql } = require("apollo-server");
 
 
 
-//server
 
-const server=new ApolloServer();
+
+//schema
+const typeDefs=gql`
+    type Curso{
+        titulo:String
+        tecnologia:String
+    }
+
+    type Query{
+        obtenerCursos: Curso
+    }
+`;
+
+const cursos = [
+    {
+        titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+        tecnologia: 'JavaScript ES6',
+    },
+    {
+        titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+        tecnologia: 'React',
+    },
+    {
+        titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+        tecnologia: 'Node.js'
+    }, 
+    {
+        titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+        tecnologia: 'React'
+    }
+];
+
+
+//resolver
+const resolvers={
+    Query:{
+        obtenerCursos:()=>cursos[0]
+    }
+}
+
+//server
+const server=new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
 //arranca server
-
 server.listen().then(({url})=>{
     console.log(`Servidor corriendo en ${url}`);
 })
